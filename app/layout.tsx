f84054399c92307bcf2d09c0575486f3d3b262f4
kennedexx <kennedex531@outlook.com>
2025-06-05 21:24:27 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,35 @@
+export const metadata = {
+  title: "Seu site",
+  description: "Landing da Art House",
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="pt-br">
+      <head>
+        {/* Pixel da Meta */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '723202246884028');
+              fbq('track', 'PageView');
+            `,
+          }}
+        ></script>
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
+
 import type React from "react"
 import type { Metadata } from "next"
 import { Montserrat, Roboto } from "next/font/google"
